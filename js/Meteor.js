178f2ce62c3c28558a26fe0900c42bf713a4f23b
Carrot Xinyu Liu <xl3138@nyu.edu>
2023-11-08 22:15:18 +0800

    backupreadme

--- js/Meteor.js
+++ js/Meteor.js
@@ -1,14 +1,32 @@
 class Meteor{
-    constructor(){
-
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.spdX = 0;
+        this.spdY = 0;
+        this.alpha = 255;
+        this.lifeSpan = floor(random(120, 360));
     }
+
     update(){
-
+        this.spdX --;
+        this.spdY --;
+        this.x += this.spdX;
+        this.y += this.spdY;
     }
+
     display(){
-
+        this.drawMeteor();
     }
+
     drawMeteor(){
-
+        push();
+        translate(this.x, this.y);
+        noStroke();
+        fill(255, this.alpha);
+        circle(0, 0, 10);
+        stroke(255);
+        pop();
     }
+
 }
--- js/Star.js
+++ js/Star.js
@@ -1,14 +1,31 @@
 class Star{
-    constructor(){
-
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.alpha = 255;
+        this.size = 10;
+        this.lifeSpan = floor(random(120, 360));
     }
     update(){
-
+        this.size ++;
     }
     display(){
-
+        this.drawMeteor();
     }
     drawStar(){
-
+        push();
+        translate(this.x, this.y);
+        fill(255, this.alpha);
+        circle(0, 0, 10);
+        pop();
+    }
+    drawRipple(){
+        push();
+        translate(this.x, this.y);
+        noFill();
+        stroke(255, this.alpha);
+        strokeWeight(3);
+        circle(0, 0, this.size);
+        pop();
     }
 }
